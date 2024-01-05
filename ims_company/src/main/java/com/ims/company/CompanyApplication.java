c22ddc73816f46a59178a6302852fc9b8a7987f7
ZHEN603 <a513515732@hotmail.com>
2024-01-05 20:35:26 +0000

    Company Entity

--- ims_company/src/main/java/com/ims/company/CompanyApplication.java
+++ ims_company/src/main/java/com/ims/company/CompanyApplication.java
@@ -1,8 +1,10 @@
 package com.ims.company;
 
+import com.ims.common.utils.IdWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 
 //1.配置springboot的包扫描
 @SpringBootApplication(scanBasePackages = "com.ims.company")
@@ -16,5 +18,9 @@ public class CompanyApplication {
      */
     public static void main(String[] args) {
         SpringApplication.run(CompanyApplication.class,args);
+    }
+    @Bean
+    public IdWorker idWorker() {
+        return new IdWorker();
     }
 }
