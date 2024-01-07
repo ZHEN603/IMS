bcc43900ef8756641d05e7e40af40bcddea0a480
ZHEN603 <a513515732@hotmail.com>
2024-01-07 21:47:30 +0000

    user Dao

---
+++ ims_user/src/main/java/com/ims/user/dao/UserDao.java
@@ -0,0 +1,8 @@
+package com.ims.user.dao;
+
+import com.ims.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface UserDao extends JpaRepository<User,String>, JpaSpecificationExecutor<User> {
+}
