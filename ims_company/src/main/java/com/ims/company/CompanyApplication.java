34440ba7bd7546a44ef3eab37412ce26ae25617d
ZHEN603 <a513515732@hotmail.com>
2024-01-07 22:30:45 +0000

    company interceptor

--- ims_company/src/main/java/com/ims/company/CompanyApplication.java
+++ ims_company/src/main/java/com/ims/company/CompanyApplication.java
@@ -1,16 +1,21 @@
 package com.ims.company;
 
 import com.ims.common.utils.IdWorker;
+import com.ims.common.utils.JwtUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
+import org.springframework.web.bind.annotation.RestController;
 
 //1.配置springboot的包扫描
 @SpringBootApplication(scanBasePackages = "com.ims")
 //2.配置jpa注解的扫描
 @EntityScan(value="com.ims.domain.company")
-
+@EnableFeignClients
 public class CompanyApplication {
 
     /**
@@ -22,5 +27,15 @@ public class CompanyApplication {
     @Bean
     public IdWorker idWorker() {
         return new IdWorker();
+    }
+
+    @Bean
+    public JwtUtils jwtUtils() {
+        return new JwtUtils();
+    }
+
+    @Bean
+    public OpenEntityManagerInViewFilter openEntityManagerInViewFilter() {
+        return new OpenEntityManagerInViewFilter();
     }
 }
---
+++ ims_company/src/main/java/com/ims/company/CompanyInterceptorConfig.java
@@ -0,0 +1,20 @@
+package com.ims.company;
+
+import com.ims.common.interceptor.JwtInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class CompanyInterceptorConfig extends WebMvcConfigurationSupport {
+    @Autowired
+    private JwtInterceptor jwtInterceptor;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor).
+                addPathPatterns("/**").
+                excludePathPatterns(""); //设置不拦截的请求地址
+    }
+}
+
