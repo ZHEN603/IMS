0d681151eef7d14a9385f6fcb6e603068a3aa067
Zhenyu Liang <a513515732@hotmail.com>
2024-02-01 18:54:30 +0000

    update product model

---
+++ ims_common_model/src/main/java/com/ims/domain/product/Product.java
@@ -0,0 +1,31 @@
+package com.ims.domain.product;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "pro_product")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product implements Serializable {
+    @Id
+    private String id;
+    private String companyId;
+    private String name;
+    private String categoryId;
+    private String categoryName;
+    private Double price;
+    private Double discount;
+    private Double cost;
+    private String description;
+    private Date createTime;
+}
+
