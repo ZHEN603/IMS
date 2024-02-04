4c41236451ff6ab036c20bbabdfbbbfc5ef1c69c
Zhenyu Liang <a513515732@hotmail.com>
2024-02-04 13:35:31 +0000

    default companyId

--- ims_common/src/main/java/com/ims/common/controller/BaseController.java
+++ ims_common/src/main/java/com/ims/common/controller/BaseController.java
@@ -7,25 +7,15 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-/**
- * @author: hyl
- * @date: 2020/01/04
- **/
 public class BaseController {
 
     public HttpServletRequest request;
     public HttpServletResponse response;
-    protected String companyId;
-    protected String companyName;
+    protected String companyId="1";
+    protected String companyName="company1";
 
     protected Claims claims;
 
-    /**
-     * 进入控制器之前执行的方法,使用shiro获取
-     *
-     * @param request  请求
-     * @param response 响应
-     */
     @ModelAttribute
     public void serResAndReq(HttpServletRequest request, HttpServletResponse response) {
         this.request = request;
