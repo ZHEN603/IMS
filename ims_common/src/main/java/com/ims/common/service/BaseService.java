f416dc0d496d1d79eb22b0ca574f73260ed78297
Zhenyu Liang <a513515732@hotmail.com>
2024-02-04 13:36:29 +0000

    update baseService

--- ims_common/src/main/java/com/ims/common/service/BaseService.java
+++ ims_common/src/main/java/com/ims/common/service/BaseService.java
@@ -2,10 +2,6 @@ package com.ims.common.service;
 
 import org.springframework.data.jpa.domain.Specification;
 
-/**
- * @author: hyl
- * @date: 2020/01/04
- **/
 public class BaseService<T> {
 
     protected Specification<T> getSpec(String companyId){
