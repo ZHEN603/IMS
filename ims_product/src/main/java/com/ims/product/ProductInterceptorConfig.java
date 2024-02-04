4d385e17046e235bc2f3256042f395365dda2a12
Zhenyu Liang <a513515732@hotmail.com>
2024-02-04 13:33:59 +0000

    product interceptor

---
+++ ims_product/src/main/java/com/ims/product/ProductInterceptorConfig.java
@@ -0,0 +1,17 @@
+package com.ims.product;
+
+import com.ims.common.interceptor.JwtInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+@Configuration
+public class ProductInterceptorConfig extends WebMvcConfigurationSupport {
+    @Autowired
+    private JwtInterceptor jwtInterceptor;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor).
+                addPathPatterns("/**");
+    }
+}
