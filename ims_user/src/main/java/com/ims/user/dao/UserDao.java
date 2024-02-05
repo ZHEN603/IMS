220bd5d7589980b8976ce311349998bd6ed7024b
Zhenyu Liang <a513515732@hotmail.com>
2024-02-05 16:24:47 +0000

    delete useless libraries in userDao

--- ims_user/src/main/java/com/ims/user/dao/UserDao.java
+++ ims_user/src/main/java/com/ims/user/dao/UserDao.java
@@ -1,6 +1,9 @@
 package com.ims.user.dao;
 
 import com.ims.domain.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
