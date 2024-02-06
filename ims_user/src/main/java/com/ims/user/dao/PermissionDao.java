210bf23f6be7afceed9f93540a76956959523db7
Zhenyu Liang <a513515732@hotmail.com>
2024-02-06 17:21:57 +0000

    update permission dao

--- ims_user/src/main/java/com/ims/user/dao/PermissionDao.java
+++ ims_user/src/main/java/com/ims/user/dao/PermissionDao.java
@@ -1,19 +1,8 @@
 package com.ims.user.dao;
 
-
-
 import com.ims.domain.user.Permission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.util.List;
-
-/**
-  * 权限数据访问接口
-  */
 public interface PermissionDao extends JpaRepository<Permission, String>, JpaSpecificationExecutor<Permission> {
-
-    List<Permission> findByTypeAndPid(int type,String pid);
-
-    List<Permission> findByTypeAndEnVisible(int type, String enVisible);
 }
