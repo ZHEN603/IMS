f67814255d56cbac288c0e9be862bb1b040c3291
Zhenyu Liang <a513515732@hotmail.com>
2024-02-08 18:31:42 +0000

    add findCompanyByPage

--- ims_company/src/main/java/com/ims/company/controller/CompanyController.java
+++ ims_company/src/main/java/com/ims/company/controller/CompanyController.java
@@ -1,73 +1,72 @@
 package com.ims.company.controller;
 
+import com.ims.common.entity.PageResult;
 import com.ims.common.entity.Result;
 import com.ims.common.entity.ResultCode;
 import com.ims.company.service.CompanyService;
 import com.ims.domain.company.Company;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 
-import java.util.List;
-
-//解决跨域问题
+//Cross domain
 @CrossOrigin
 @RestController
 @RequestMapping(value="/company")
 public class CompanyController {
-
     @Autowired
     private CompanyService companyService;
 
-    //保存用户
+
     @RequestMapping(value="",method = RequestMethod.POST)
-    public Result save(@RequestBody Company company)  {
-        //业务操作
-        companyService.add(company);
+    public Result save(@RequestBody Company company) {
+        companyService.save(company);
         return new Result(ResultCode.SUCCESS);
     }
 
-    //根据id更新企业信息
+    //Update
     @RequestMapping(value = "/{id}",method = RequestMethod.PUT)
     public Result update(@PathVariable(value="id") String id, @RequestBody Company company ) {
-        //业务操作
         company.setId(id);
         companyService.update(company);
         return new Result(ResultCode.SUCCESS);
     }
 
-    //根据id删除企业
+    //Delete
     @RequestMapping(value="/{id}",method = RequestMethod.DELETE)
     public Result delete(@PathVariable(value="id") String id) {
         companyService.deleteById(id);
         return new Result(ResultCode.SUCCESS);
     }
 
-    //根据id查询企业
+    //Find company by ID
     @RequestMapping(value="/{id}",method = RequestMethod.GET)
     public Result findCompanyById(@PathVariable(value="id") String id){
-        Company company = companyService.findById(id);
-        Result result = new Result(ResultCode.SUCCESS);
-        result.setData(company);
-        return result;
+        return new Result(ResultCode.SUCCESS, companyService.findById(id));
     }
 
-    //查询全部企业列表
+    //Find all companies
     @RequestMapping(value="",method = RequestMethod.GET)
     public Result findAll() {
-        List<Company> list = companyService.findAll();
-        Result result = new Result(ResultCode.SUCCESS);
-        result.setData(list);
-        return result;
+        return new Result(ResultCode.SUCCESS, companyService.findAll());
     }
 
-    //更新企业状态
+    //Find all companies by page
+    @RequestMapping(value = "/list" , method = RequestMethod.GET)
+    public Result findAll(int page, int pagesize, String keyword){
+        Page<Company> pageUser = companyService.findAll(page, pagesize, keyword);
+        PageResult<Company> pageResult = new PageResult<>(pageUser.getTotalElements(),pageUser.getContent());
+        return new Result(ResultCode.SUCCESS , pageResult);
+    }
+
+    //Update company's state
     @RequestMapping(value="/{id}/state/{state}",method = RequestMethod.PUT)
-    public Result findAll(@PathVariable(value = "id") String id ,
-                          @PathVariable(value = "state") int state) {
+    public Result updateState(@PathVariable(value = "id") String id ,@PathVariable(value = "state") Integer state) {
         Company company = companyService.findById(id);
         company.setState(state);
-        companyService.save(company);
+        companyService.update(company);
         return new Result(ResultCode.SUCCESS);
     }
 }
