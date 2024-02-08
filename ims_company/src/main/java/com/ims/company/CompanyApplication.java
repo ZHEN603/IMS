8f54378cedb6d95938c2bfb97ffe9249544f9e3b
Zhenyu Liang <a513515732@hotmail.com>
2024-02-08 18:31:14 +0000

    update company application

--- ims_company/src/main/java/com/ims/company/CompanyApplication.java
+++ ims_company/src/main/java/com/ims/company/CompanyApplication.java
@@ -1,26 +1,19 @@
 package com.ims.company;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ims.common.utils.IdWorker;
 import com.ims.common.utils.JwtUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
-import org.springframework.web.bind.annotation.RestController;
 
-//1.配置springboot的包扫描
 @SpringBootApplication(scanBasePackages = "com.ims")
-//2.配置jpa注解的扫描
 @EntityScan(value="com.ims.domain.company")
 @EnableFeignClients
 public class CompanyApplication {
-
-    /**
-     * 启动方法
-     */
     public static void main(String[] args) {
         SpringApplication.run(CompanyApplication.class,args);
     }
@@ -38,4 +31,10 @@ public class CompanyApplication {
     public OpenEntityManagerInViewFilter openEntityManagerInViewFilter() {
         return new OpenEntityManagerInViewFilter();
     }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
 }
