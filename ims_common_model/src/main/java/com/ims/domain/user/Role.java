b32e193369b70d0acac7ed3f22f256cdccb932d0
Zhenyu Liang <a513515732@hotmail.com>
2024-02-13 21:18:12 +0000

    update role model

--- ims_common_model/src/main/java/com/ims/domain/user/Role.java
+++ ims_common_model/src/main/java/com/ims/domain/user/Role.java
@@ -1,42 +1,40 @@
 package com.ims.domain.user;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "pe_role")
+@Table(name = "u_role")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Role implements Serializable {
-    private static final long serialVersionUID = 594829320797158219L;
+    private static final long serialVersionUID = 1L;
     @Id
     private String id;
-    /**
-     * 角色名
-     */
     private String name;
-    /**
-     * 说明
-     */
     private String description;
-    /**
-     * 企业id
-     */
     private String companyId;
+    private Integer state;
 
     @JsonIgnore
-    @ManyToMany(mappedBy="roles")  //不维护中间表
-    private Set<User> users = new HashSet<User>(0);//角色与用户   多对多
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<User>(0);
 
 
     @JsonIgnore
     @ManyToMany
-    @JoinTable(name="pe_role_permission",
+    @JoinTable(name="u_role_permission",
             joinColumns={@JoinColumn(name="role_id",referencedColumnName="id")},
             inverseJoinColumns={@JoinColumn(name="permission_id",referencedColumnName="id")})
-    private Set<Permission> permissions = new HashSet<Permission>(0);//角色与模块  多对多
+    private Set<Permission> permissions = new HashSet<Permission>(0);
 }
