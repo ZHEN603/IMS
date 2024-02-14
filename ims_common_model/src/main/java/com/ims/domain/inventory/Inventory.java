da1354c8c143959a298d63ad75b970ecd5843dfc
Zhenyu Liang <a513515732@hotmail.com>
2024-02-14 22:49:32 +0000

    add inventory model

---
+++ ims_common_model/src/main/java/com/ims/domain/inventory/Inventory.java
@@ -0,0 +1,24 @@
+package com.ims.domain.inventory;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "in_inventory")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Inventory {
+    @Id
+    private String id;
+    private String companyId;
+    private Integer quantity;
+    private Integer lowStock;
+    private Integer state;
+
+    private String categoryId;
+    private String productId;
+    private String productName;
+}
