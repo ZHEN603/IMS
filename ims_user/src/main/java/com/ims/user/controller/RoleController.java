9a22128c039629161dac48277a7633cd48062a6a
Zhenyu Liang <a513515732@hotmail.com>
2024-02-14 21:19:38 +0000

    add findRolesByUserId

--- ims_user/src/main/java/com/ims/user/controller/RoleController.java
+++ ims_user/src/main/java/com/ims/user/controller/RoleController.java
@@ -5,15 +5,12 @@ import com.ims.common.entity.PageResult;
 import com.ims.common.entity.Result;
 import com.ims.common.entity.ResultCode;
 import com.ims.domain.user.Role;
-import com.ims.domain.user.RoleAndUserRelations;
 import com.ims.domain.user.response.RoleResult;
 import com.ims.user.service.RoleService;
-import com.ims.user.service.UserAndRoleRelationsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
-import jakarta.annotation.Resource;
 import java.util.List;
 import java.util.Map;
 
@@ -25,32 +22,12 @@ import java.util.Map;
 //解决跨域
 @CrossOrigin
 @RestController
-@RequestMapping(value = "/sys")
+@RequestMapping(value = "/user")
 public class RoleController extends BaseController {
 
-    @Resource
+    @Autowired
     private RoleService roleService;
 
-    @Resource
-    private UserAndRoleRelationsService userAndRoleRelationsService;
-
-    /**
-     * 分配权限
-     */
-    @RequestMapping(value = "/role/assignPrem" , method = RequestMethod.PUT)
-    public Result save(@RequestBody Map<String,Object> map){
-
-        //获取被分配的角色id
-        String roleId = (String) map.get("id");
-        //获取到权限的id列表
-        List<String> permIds = (List<String>) map.get("permIds");
-        //调用service完成权限分配
-        roleService.assignPerms(roleId , permIds);
-
-        return new Result(ResultCode.SUCCESS);
-    }
-
-    //添加角色
     @RequestMapping(value = "/role", method = RequestMethod.POST)
     public Result add(@RequestBody Role role) throws Exception {
         role.setCompanyId(companyId);
@@ -58,23 +35,19 @@ public class RoleController extends BaseController {
         return Result.SUCCESS();
     }
 
-    //更新角色
     @RequestMapping(value = "/role/{id}", method = RequestMethod.PUT)
     public Result update(@PathVariable(name = "id") String id, @RequestBody Role role) throws Exception {
+        role.setId(id);
         roleService.update(role);
         return Result.SUCCESS();
     }
 
-    //删除角色
     @RequestMapping(value = "/role/{id}", method = RequestMethod.DELETE)
     public Result delete(@PathVariable(name = "id") String id) throws Exception {
         roleService.delete(id);
         return Result.SUCCESS();
     }
 
-    /**
-     * 根据ID获取角色信息
-     */
     @RequestMapping(value = "/role/{id}", method = RequestMethod.GET)
     public Result findById(@PathVariable(name = "id") String id) throws Exception {
         Role role = roleService.findById(id);
@@ -82,11 +55,8 @@ public class RoleController extends BaseController {
         return new Result(ResultCode.SUCCESS,roleResult);
     }
 
-    /**
-     * 分页查询角色
-     */
     @RequestMapping(value = "/role", method = RequestMethod.GET)
-    public Result findByPage(int page,int pagesize,Role role) throws Exception {
+    public Result findByPage(int page,int pagesize) throws Exception {
         Page<Role> searchPage = roleService.findByPage(companyId, page, pagesize);
         PageResult<Role> pr = new PageResult(searchPage.getTotalElements(),searchPage.getContent());
         return new Result(ResultCode.SUCCESS,pr);
@@ -94,25 +64,19 @@ public class RoleController extends BaseController {
 
     @RequestMapping(value="/role/list" ,method=RequestMethod.GET)
     public Result findAll() throws Exception {
-        List<Role> roleList = roleService.findAll(companyId);
-        return new Result(ResultCode.SUCCESS,roleList);
+        return new Result(ResultCode.SUCCESS,roleService.findAll(companyId));
     }
 
-    /**
-     * 根据用户id获取全部权限
-     * @param id
-     * @return
-     */
     @RequestMapping(value = "/role/userId/{id}", method = RequestMethod.GET)
     public Result findRolesByUserId(@PathVariable(name = "id") String id) {
-        List<RoleAndUserRelations> roleByUserId = userAndRoleRelationsService.findRoleByUserId(id);
-        if (!ObjectUtils.isEmpty(roleByUserId)){
-            List<Role> roles = userAndRoleRelationsService.getRoleDetailByRoleId(roleByUserId);
-            if (!ObjectUtils.isEmpty(roles)){
-                return new Result(ResultCode.SUCCESS , roles);
-            }
-        }
-        return new Result(ResultCode.SUCCESS);
+        return new Result(ResultCode.SUCCESS, roleService.findRolesByUserId(id));
     }
 
+    @RequestMapping(value = "/role/assignPrem" , method = RequestMethod.PUT)
+    public Result save(@RequestBody Map<String,Object> map){
+        String roleId = (String) map.get("id");
+        List<String> permIds = (List<String>) map.get("permIds");
+        roleService.assignPerms(roleId , permIds);
+        return new Result(ResultCode.SUCCESS);
+    }
 }
--- ims_user/src/main/java/com/ims/user/dao/RoleDao.java
+++ ims_user/src/main/java/com/ims/user/dao/RoleDao.java
@@ -4,6 +4,9 @@ import com.ims.domain.user.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 
 public interface RoleDao extends JpaRepository<Role,String>, JpaSpecificationExecutor<Role> {
+    List<Role> findByUsersId(String userId);
 }
