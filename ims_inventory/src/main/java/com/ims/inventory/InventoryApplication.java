891f96772faeb5bad8953c1149cfd957a87caff0
Zhenyu Liang <a513515732@hotmail.com>
2024-02-14 22:50:32 +0000

    add inventory app

---
+++ ims_inventory/src/main/java/com/ims/inventory/InventoryApplication.java
@@ -0,0 +1,34 @@
+package com.ims.inventory;
+
+import com.ims.common.utils.IdWorker;
+import com.ims.common.utils.JwtUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
+
+@SpringBootApplication(scanBasePackages = "com.ims")
+@EntityScan(value="com.ims.domain.inventory")
+@EnableFeignClients
+public class InventoryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryApplication.class,args);
+    }
+    @Bean
+    public IdWorker idWorker() {
+        return new IdWorker();
+    }
+
+    @Bean
+    public JwtUtils jwtUtils() {
+        return new JwtUtils();
+    }
+
+    @Bean
+    public OpenEntityManagerInViewFilter openEntityManagerInViewFilter() {
+        return new OpenEntityManagerInViewFilter();
+    }
+
+}
