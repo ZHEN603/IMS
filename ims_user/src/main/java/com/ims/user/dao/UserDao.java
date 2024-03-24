6e589aca67d2d77b61892f22f22b66995182e7ec
Zhenyu Liang <a513515732@hotmail.com>
2024-03-24 13:06:36 +0000

    Delete User Interceptor

--- ims_user/src/main/java/com/ims/user/dao/UserDao.java
+++ ims_user/src/main/java/com/ims/user/dao/UserDao.java
@@ -8,4 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface UserDao extends JpaRepository<User,String>, JpaSpecificationExecutor<User> {
+    User findByEmail(String email);
 }
