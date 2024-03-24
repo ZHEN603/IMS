968a28cc863baf4e57b5d9f351621610d91008b5
Zhenyu Liang <a513515732@hotmail.com>
2024-03-24 13:10:20 +0000

    Create product MQ

--- ims_product/src/main/java/com/ims/product/mq/ProductMessageReceiver.java
+++ ims_product/src/main/java/com/ims/product/mq/ProductMessageReceiver.java
@@ -1,8 +1,7 @@
 package com.ims.product.mq;
-import com.ims.domain.product.Product;
 import com.ims.product.service.ProductService;
+import jakarta.annotation.Resource;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -10,18 +9,17 @@ import java.util.Map;
 @Component
 public class ProductMessageReceiver {
 
-    @Autowired
-    ProductService productService;
+    @Resource
+    private ProductService productService;
 
     @RabbitListener(queues = "productRollbackQueue")
-    public void handleProductCreate(Map<String, String> map) {
+    public void handleProductRollback(Map<String, String> map) {
         switch (map.get("route")){
             case "create":
-                System.out.println("create message: " + map.get("id"));
                 productService.deleteById(map.get("id"));
                 break;
             case "delete", "update":
-                System.out.println("delete message: " + map.get("id"));
+                productService.rollBack(map.get("id"));
                 break;
             default:
                 break;
--- ims_product/src/main/java/com/ims/product/mq/ProductMessageSender.java
+++ ims_product/src/main/java/com/ims/product/mq/ProductMessageSender.java
@@ -1,28 +1,25 @@
 package com.ims.product.mq;
-import com.ims.common.message.ProductMessage;
 import com.ims.domain.product.Product;
+import jakarta.annotation.Resource;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class ProductMessageSender {
-    private final RabbitTemplate rabbitTemplate;
-
-    @Autowired
-    public ProductMessageSender(RabbitTemplate rabbitTemplate) {
-        this.rabbitTemplate = rabbitTemplate;
-    }
+    @Resource
+    private RabbitTemplate rabbitTemplate;
 
     public void sendProductUpdateMessage(Product message) {
         rabbitTemplate.convertAndSend("productExchange", "product.update", message);
     }
 
-    public void sendProductDeleteMessage(Product message) {
+    public void sendProductDeleteMessage(String message) {
         rabbitTemplate.convertAndSend("productExchange", "product.delete", message);
     }
 
-    public void sendProductCreateMessage(Product message) {
+    public void sendProductCreateMessage(Map<String, Object> message) {
         rabbitTemplate.convertAndSend("productExchange", "product.create", message);
     }
 }
