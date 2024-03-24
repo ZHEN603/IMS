8bd1b553fcbb141a01ce72c8ae6e062a820d949b
Zhenyu Liang <a513515732@hotmail.com>
2024-03-24 13:12:21 +0000

    delete app sub interceptors

--- ims_inventory/src/main/java/com/ims/inventory/client/ProductFeignClient.java
+++ ims_inventory/src/main/java/com/ims/inventory/client/ProductFeignClient.java
@@ -1,19 +1,13 @@
 package com.ims.inventory.client;
 
-import com.ims.common.entity.Result;
-import com.ims.domain.product.Product;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-import java.util.Map;
 
-//@FeignClient("ims-product")
 @FeignClient(name = "ims-product", url = "http://ims-product:9003")
 public interface ProductFeignClient {
-    @RequestMapping(value="product/category/child/{id}",method = RequestMethod.GET)
-    public List<String> findAllChildIds(@PathVariable(value="id") String id, @PathVariable(value="companyId") String companyId);
+    @GetMapping("/product/category/child/{id}")
+    public List<String> findAllChildIds(@PathVariable(value="id") String id);
 }
