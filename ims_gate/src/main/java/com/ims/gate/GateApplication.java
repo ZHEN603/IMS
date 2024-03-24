06061fc787da2d9af12de393906970d628f24cbf
Zhenyu Liang <a513515732@hotmail.com>
2024-03-24 18:40:10 +0000

    update gate application

--- ims_gate/src/main/java/com/ims/gate/GateApplication.java
+++ ims_gate/src/main/java/com/ims/gate/GateApplication.java
@@ -1,13 +1,26 @@
 package com.ims.gate;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ims.common.utils.JwtUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = "com.ims")
+//@EnableDiscoveryClient
 public class GateApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GateApplication.class);
     }
+    @Bean
+    public JwtUtils jwtUtils() {
+        return new JwtUtils();
+    }
 
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
 }
